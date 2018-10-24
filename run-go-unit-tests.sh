8232f58fe7be30d092b367e61d6bacb174308578
Alexandre Vallières-Lagacé <alexandre.valliereslagace@gmail.com>
2018-10-24 11:10:43 -0400

    Fix

--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -5,6 +5,6 @@ go test -tags=unit -timeout 30s -short -v ${FILES}
 
 returncode=$?
 if [ $returncode -ne 0 ]; then
-  echo "unit tests failed" >&2
-  exit $rc
+  echo "unit tests failed"
+  exit 1
 fi
